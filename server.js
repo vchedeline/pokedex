b7165b7537e9072bac0a1f2e5f5e6401b46616ba
chedeline <vchedeline@gmail.com>
2022-04-09 18:41:42 -0400

    configures REST routes

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const app = express();
 const PORT = process.env.PORT || 8008;
 const morgan = require("morgan");
 const methodOverride = require("method-override");
+const pokemon = require("./models/pokemon.js");
 
 app.use(express.urlencoded({ extended: false }));
 app.use("/public", express.static("public"));
@@ -14,6 +15,35 @@ app.use(methodOverride("_method"));
 app.get("/", (req, res) => {
   res.render("main.ejs");
 });
+
+// Index Route - GET
+app.get("/pokemon", (req, res) => {
+  res.send("I am INDEX!")
+})
+// Show Route - GET
+app.get("/pokemon/:id", (req, res) => {
+  res.send(`"I am SHOW! @${req.params.id}`)
+})
+// New Route - GET
+app.get("/pokemon/new", (req, res) => {
+  res.send("I am NEW!")
+})
+// Edit Route - GET
+app.get("/pokemon/:id/edit", (req, res) => {
+  res.send("I am EDIT!")
+})
+// Create Route - POST
+app.post("/pokemon", (req, res) => {
+  res.send("Let's CREATE!")
+})
+// Update Route - PUT
+app.put("/pokemon/:id", (req, res) => {
+  res.send("Let's UPDATE!")
+})
+// Delete Route - DELETE
+app.delete("/pokemon/:id", (req, res) => {
+  res.send("Let's DELETE!")
+})
 
 app.listen(PORT, () => {
   console.log(`Port ${PORT} now in session...`);
